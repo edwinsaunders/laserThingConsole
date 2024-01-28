65a290ff009ac4141ddea4e0b708c5f22a8b285d
edwinsaunders <33383744+edwinsaunders@users.noreply.github.com>
2024-01-27 22:53:26 -0500

    Ran a succesful build using cmake
    Successfully built project on linux using cmake.  Had to correct cmake version.  First attempt failed due to wrong cmake version (3.28.1 incorrect, changed to 3.25.1)

---
+++ build/CMakeFiles/laserThingConsole.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laserThingConsole.dir/src/getFirstSquare.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/getFirstSquare.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/getNextSquare.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/getNextSquare.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/main.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/main.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/printZeroGrid.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/printZeroGrid.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/setGridPattern.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/setGridPattern.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/squareParamSetup.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/squareParamSetup.cpp.o.d"
+  "CMakeFiles/laserThingConsole.dir/src/visualize.cpp.o"
+  "CMakeFiles/laserThingConsole.dir/src/visualize.cpp.o.d"
+  "laserThingConsole"
+  "laserThingConsole.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laserThingConsole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/laserThingConsole.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for laserThingConsole.
+# This may be replaced when dependencies are built.
